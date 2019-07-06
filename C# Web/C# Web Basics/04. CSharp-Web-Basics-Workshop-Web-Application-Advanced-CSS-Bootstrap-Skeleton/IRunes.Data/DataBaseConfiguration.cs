d3f4617c2b9845d8de79cdd53dfc22eaaaac4f30
Daniel Bankov <bankovdaniel@gmail.com>
2019-07-06 12:34:05 +0300

    Remove unnecessary files

--- C# Web/C# Web Basics/04. CSharp-Web-Basics-Workshop-Web-Application-Advanced-CSS-Bootstrap-Skeleton/IRunes.Data/DataBaseConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace IRunes.Data
-{
-    public class DataBaseConfiguration
-    {
-        public const string ConnectionString = @"Server=DESKTOP-SUIN9FI\SQLEXPRESS;DataBase=IRunesDB;Integrated Security=True";
-    }
-}

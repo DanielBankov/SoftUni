3eeacb54e7b7cc8d2519c30b25258a0f9e2e6245
Daniel Bankov <bankovdaniel@gmail.com>
2019-07-17 14:25:28 +0300

    Rename folders Resources -> FrontEnd & Messages.App -> Messages

---
+++ C# Web/ASP.NET Core MVC/Web API/Messages/Messages.Data/MessageDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Messages.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Messages.Data
+{
+    public class MessageDbContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+
+        public MessageDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
